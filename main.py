c006e3bd5bab1fa0df1a75b9137aa6820a26b21f
genebit <genebitara@gmail.com>
2021-07-16 18:44:02 +0800

    Updates

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 
 import pyautogui
 import keyboard
-import sys
 
 mouse_position = None
 
@@ -11,10 +10,10 @@ while running:
         mouse_position = pyautogui.position()
 
         print('Mouse is at: ({0} {1})'.format(mouse_position.x, mouse_position.y))
-
-        # if keyboard.read_hotkey():
-        #     print('A key is pressed')
-        #     break
+        
+        if keyboard.read_hotkey():
+            print('A key is pressed')
+            break
 
     except Exception as err:
         print('An error occured', err)
