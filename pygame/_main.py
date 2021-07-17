37ecd20325359729491e643961e11c23645f3d53
genebit <genebitara@gmail.com>
2021-07-17 18:32:04 +0800

    Implemented the gettime module for the fake clock to main

--- pygame/_main.py
+++ pygame/_main.py
@@ -2,14 +2,20 @@
 import pygame
 import sys
 
+import _gettime
+
+time = _gettime.GetTime()
+
+pygame.init()
+
 window = pygame.display.set_mode((1366, 768), pygame.FULLSCREEN | pygame.SCALED)
 clock = pygame.time.Clock()
 
 fake_window = pygame.image.load('./pygame/img/windows.PNG')
 
 # FIXME: Font not initialized
-font = pygame.font.SysFont('Roboto', 32)
-clock =  font.render('10', True, (255, 255, 255))
+font = pygame.font.SysFont('Roboto', 130)
+clock =  font.render(time.current_time, True, (255, 255, 255))
 clock_rect = clock.get_rect()
 clock_rect.center = (window.get_width()/2, window.get_height()/2 -20)
 
@@ -21,5 +27,6 @@ while True:
             sys.exit()
 
     window.blit(fake_window, (0, 0))
+    window.blit(clock, clock_rect)
 
     pygame.display.update()
